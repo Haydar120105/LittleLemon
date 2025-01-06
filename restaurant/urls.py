d61a8961b2334385a1a2f114eb01fe14c8396ef2
Haydar <hay.selman@outlook.de>
2025-01-06 16:33:32 +0100

    "Authenticating with DRF and DJOSER

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token  
 
 urlpatterns = [
     path("menu/", views.MenuItemview.as_view(), name="menu"),
     path("menu/<int:pk>/", views.SingleMenuItemview.as_view(), name="singlemenuitem"),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -7,16 +7,21 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 
 class MenuItemview(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
-    serializer_class = MenuSerializer(many=True)
+    serializer_class = MenuSerializer
+    
+
 
 class SingleMenuItemview(generics.RetrieveUpdateAPIView):
     queryset = Menu.objects.all()
