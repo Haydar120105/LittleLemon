ff677d9bf9210bf5ca14a9be9040033c346bfe97
Haydar <hay.selman@outlook.de>
2025-01-06 14:12:24 +0100

    "Module 2 Coursera Django for Everybody"

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("menu/", views.MenuItemview.as_view(), name="menu"),
+    path("menu/<int:pk>/", views.SingleMenuItemview.as_view(), name="singlemenuitem"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Booking, Menu
+
+from .serializers import BookingSerializer, MenuSerializer
+
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+from rest_framework.response import Response
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuItemview(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer(many=True)
+
+class SingleMenuItemview(generics.RetrieveUpdateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
